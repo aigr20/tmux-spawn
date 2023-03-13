480935d8571ffa2c72df83dbcf19be289c41b06a
Wilhelm Gren <wilhelmgren@outlook.com>
2023-03-13 19:51:08 +0100

    Set default config location in XDG_CONFIG_HOME

--- tmux_spawn_types/cli_arguments.py
+++ tmux_spawn_types/cli_arguments.py
@@ -1,3 +1,4 @@
+import os
 from argparse import Namespace
 
 
@@ -5,4 +6,6 @@ class CLIArguments(Namespace):
     session_name: str
 
     def __init__(self) -> None:
-        self.config = "sample-config.json"
+        home = os.getenv("HOME")
+        xdg_conf_home = os.getenv("XDG_CONFIG_HOME", f"{home}/.config")
+        self.config = f"{xdg_conf_home}/tmux-spawn/spawn-config.json"
